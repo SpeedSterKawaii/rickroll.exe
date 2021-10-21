cbb80ff9bdfef78073bf4032254722760ecd8ac0
SpeedSterKawaii <72403623+SpeedSterKawaii@users.noreply.github.com>
2021-10-21 10:35:43 -0400

    Create RickIntelCore.cs

---
+++ RickIntelCore.cs
@@ -0,0 +1,13 @@
+namespace ok
+{
+public void StartBeingAnnoying()
+{
+Process.Start("rickroll link");
+//now start
+MessageBox.Show("Ready to get free rabax?", "no cap");
+while (true)
+{
+Process.Start("rickroll");//hah
+}
+}
+}
